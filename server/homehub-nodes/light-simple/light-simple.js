e549ca19bfeb8e1e7fa31c826992ba7ccbfd2e3d
Darran Rimron <darran.rimron@beatsystems.com>
2015-10-20 12:09:43 +0100

    Adding a cleanup stub

--- server/homehub-nodes/light-simple/light-simple.js
+++ server/homehub-nodes/light-simple/light-simple.js
@@ -1,11 +1,18 @@
 module.exports = function(RED) {
     function HomeHubNode(config) {
+	console.log("HOMEHUBNODE");
+	console.log(config);
         RED.nodes.createNode(this,config);
         var node = this;
         this.on('input', function(msg) {
-            msg.payload = "p[oopmsg.payload";
+            msg.payload = "msg.payload";
             node.send(msg);
         });
+	this.on('close', function() {
+    		// tidy up any state
+	});
+	console.log(node);
+	console.log("HOMEHUBNODEINITED");
     }
     RED.nodes.registerType("light-simple",HomeHubNode);
 }
